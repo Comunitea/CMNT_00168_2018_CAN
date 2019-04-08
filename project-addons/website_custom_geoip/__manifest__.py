c6568c0f8699e078a4c19965efaa81dd898b146b
neo-oien <i@oien.ru>
2019-04-08 15:34:44 +0200

    [MERGE]

---
+++ project-addons/website_custom_geoip/__manifest__.py
@@ -0,0 +1,22 @@
+# © 2019 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Website geoip customizations',
+    'version': '11.0.1.0.0',
+    'summary': '',
+    'category': 'Website',
+    'author': 'comunitea',
+    'website': 'www.comunitea.com',
+    'license': 'AGPL-3',
+    'depends': [
+        'website_sale_stock',
+    ],
+    'data': [
+        'views/stock_warehouse.xml',
+        'views/res_country_group.xml',
+        'views/website_templates.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ project-addons/website_custom_geoip/models/website.py
@@ -0,0 +1,30 @@
+# © 2019 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import models
+from odoo.http import request
+
+
+class Website(models.Model):
+    _inherit = 'website'
+
+    def get_website_available(self, not_current=False):
+        domain = [('website_available', '=', True)]
+        if not_current:
+            domain.append(('id', '!=', self.get_current_country().id))
+        return self.env['res.country.group'].search(domain)
+
+    def get_current_country(self):
+        country_code = request.session['geoip'].get('country_code')
+        if not country_code:
+            request.session['geoip']['country_code'] = \
+                self.company_id.country_id.code
+            return self.get_current_country()
+        return self.env['res.country.group'].search(
+            [('country_code', '=', country_code)])
+
+    def _prepare_sale_order_values(self, partner, pricelist):
+        res = super()._prepare_sale_order_values(partner, pricelist)
+        country = self.get_current_country()
+        res['warehouse_id'] = self.env['stock.warehouse'].sudo(
+            ).get_warehouse_id(country.country_code).id
+        return res
