1d3ddb104830e52f3f8d701efd13c0a8b988b54c
neo-oien <i@oien.ru>
2019-06-04 14:09:09 +0200

    [UPD] Recetas: breadcrumbs

--- project-addons/website_base_multi_can/controllers/product_recipe.py
+++ project-addons/website_base_multi_can/controllers/product_recipe.py
@@ -3,8 +3,6 @@
 from odoo import http
 from odoo.http import request
 
-import ipdb
-
 
 class RecipeController(http.Controller):
     @http.route(['/recipes'], type='http', auth='public', website=True)
@@ -12,18 +10,12 @@ class RecipeController(http.Controller):
         recipes = request.env['product.recipe']
         domain = []
         values = {'recipe_list': recipes.search(domain, order='website_sequence desc')}
-
-        # ipdb.set_trace()
-
         return request.render('website_base_multi_can.page_recipe_list', values)
 
     @http.route(['/recipes/<path:path>'], type='http', auth='public', website=True)
     def get_one_recipe(self, path):
         recipes = request.env['product.recipe']
         recipe = recipes.search([('slug', '=', path)], limit=1)
-
-        # ipdb.set_trace()
-
         if recipe:
             values = {'recipe': recipe}
             result = request.render('website_base_multi_can.page_one_recipe', values)
--- project-addons/website_base_multi_can/models/website.py
+++ project-addons/website_base_multi_can/models/website.py
@@ -5,9 +5,10 @@
 #
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import http, api, models, fields
+from odoo import http, api, models, fields, _
 from odoo.http import request
 from odoo.addons.seo_base.models.settings import _default_website
+from odoo.addons.breadcrumbs_base.models.breadcrumbs import _generate_one
 
 
 class Website(models.Model):
@@ -22,6 +23,17 @@ class Website(models.Model):
     social_instagram = fields.Char(string='Instagram Account')
     email = fields.Char(string='Website Email')
     phone = fields.Char(string='Website Phone')
+
+    @api.multi
+    def generate_breadcrumbs(self, main_object, website):
+        breadcrumbs = self.env['breadcrumbs_base.crumb'].sudo()
+
+        if main_object._name == 'product.recipe':
+            breadcrumbs += _generate_one(_("Recipes"), '/recipes', False)
+            breadcrumbs += _generate_one(main_object.title, '/recipes/%s' % main_object.slug, True)
+            return breadcrumbs
+
+        return super(Website, self).generate_breadcrumbs(main_object, website)
 
 
 class ResConfigSettings(models.TransientModel):
