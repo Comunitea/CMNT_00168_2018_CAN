d236be336832f8f4a7881a14e1795c818b226f8a
ruben <ruben@comunitea.com>
2019-11-14 11:12:37 +0100

    [FIX] PPR Products Per Row

--- project-addons/website_base_multi_can/controllers/__init__.py
+++ project-addons/website_base_multi_can/controllers/__init__.py
@@ -1,24 +1,7 @@
 # -*- coding: utf-8 -*-
-#
-##############################################################################
-#
-#    License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-#    © 2019 Comunitea - Ruben Seijas <ruben@comunitea.com>
-#    © 2019 Comunitea - Pavel Smirnov <pavel@comunitea.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
 
 from . import product_recipe, redsys, website
+from odoo.addons.website_sale.controllers import main
+# PPG = 20  # Products Per Page
+# PPR = 4   # Products Per Row
+main.PPR = 1000
